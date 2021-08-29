a150df11debd3fe9d60058efdb67ba69a224a1f9
azke <89734190+aziike@users.noreply.github.com>
2021-08-29 13:02:29 +0000

    nothing

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ number = random.randint(1, 10)
 player_name = input("Hello, What's your name?")
 number_of_guesses = 0
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
-#hdhsdhw
-#kckjkcj
-#dcdjh
+
 while number_of_guesses < 5:
     guess = int(input())
     number_of_guesses += 1
@@ -20,5 +18,3 @@ if guess == number:
     print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
 else:
     print('You did not guess the number, The number was ' + str(number))
-
-    #shdskjs
