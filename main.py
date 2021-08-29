44e30102016fbc5b1e4ce1d9b39b29b309180148
azke <>
2021-08-29 12:43:43 +0000

    adding schodo

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ number = random.randint(1, 10)
 player_name = input("Hello, What's your name?")
 number_of_guesses = 0
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
-
+#hdhsdhw
+#kckjkcj
+#dcdjh
 while number_of_guesses < 5:
     guess = int(input())
     number_of_guesses += 1
@@ -18,3 +20,5 @@ if guess == number:
     print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
 else:
     print('You did not guess the number, The number was ' + str(number))
+
+    #shdskjs
